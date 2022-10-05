0e2c77c69240ea06df86913792adf4dbf573ca3e
unser.name <Hoangvo981@gmail.com>
2022-10-05 21:07:58 +0700

    2-example

--- 2-example/src/App.tsx
+++ 2-example/src/App.tsx
@@ -4,9 +4,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello word!
-    </div>
+   <>
+  <div className='heaDer'>
+  <h1>The Pulpit Rock</h1>
+  </div>
+   <div>
+    <ul className='coL1'>
+      <li>The driver</li>
+      <hr/>
+      <li>The walk</li>
+      <hr/>
+      <li>The return</li>
+      <hr/>
+      <li>The End</li>
+      <hr/>
+    </ul>
+   </div>
+   <div>
+    <h2>The walk</h2>
+    <p>the walk is the</p>
+   </div>
+   <div>
+    <h2>What?</h2>
+    <p></p>
+    <h2>Whare?</h2>
+    <p></p>
+    <h2>Price?</h2>
+   </div>
+   </>
   );
 }
 export default App;
